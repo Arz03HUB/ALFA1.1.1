6c91fe09e4c29922708f101216abbd349fb460d9
lameraverdura2y3-alt <lameraverdura2y3@gmail.com>
2026-09-02 18:40:22 -0400

    ini
    ini

---
+++ alfa_pr_1/carlos_fl/cdlores2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alfa_pr_1.carlos_fl
+{
+    public  class cdlores2
+    {
+        public static void main (string[] args)
+        {
+            Console.WriteLine("Ingrese el valor para num1");
+            int uno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor para num2");
+            int dos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("la suma es :" + uno +" " + dos);
+        }
+    }
+}
---
+++ alfa_pr_1/carlos_fl/cflores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alfa_pr_1.carlos_fl
+{
+    public class cflores
+    {
+        public static void Main (string[] args)
+        {
+        Console.WriteLine("hola mundo");
+
+     } 
+ }
+}
+
