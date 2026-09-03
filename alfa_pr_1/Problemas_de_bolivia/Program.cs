1891605c640e7ba2b5386ed68f850544c13c259b
golf-group1 <tecnologiamq12@gmail.com>
2026-09-03 15:15:46 -0400

    init
    commi

---
+++ alfa_pr_1/Problemas_de_bolivia/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problemas_de_bolivia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
