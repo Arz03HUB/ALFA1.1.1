3e18bf92c9fc88583de08b06a3851b275bc7442f
lameraverdura2y3-alt <lameraverdura2y3@gmail.com>
2026-09-03 14:34:46 -0400

    imp
    imp

---
+++ alfa_pr_1/Clase04/prueba.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace alfa_pr_1.Clase04
+{
+    public class prueba
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Ingrese el valor para num1");
+            int uno = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el valor para num2");
+            int dos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("la suma es :" + uno + " " + dos);
+        }
+    }
+}
