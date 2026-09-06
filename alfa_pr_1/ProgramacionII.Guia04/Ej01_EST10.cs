b034cc0d761638fd663326bae567513062292e99
golf-group1 <tecnologiamq12@gmail.com>
2026-09-05 22:24:52 -0400

    init
    commi

---
+++ alfa_pr_1/ProgramacionII.Guia04/Ej01_EST10.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace ProgramacionII.Guia04
+{
+    public class Ej01_EST10
+    {
+        public static void Main(string[] args)
+        {
+            const int TotalMuestreo = 7;
+
+            int puntos = 1;           // inicializacion
+
+            while (puntos <= 7)        // condicion 
+            {
+                Console.WriteLine("Punto de muestreo N°: " + puntos);
+                puntos++;     // actulizacion 
+            }
+        }
+    }
+}
