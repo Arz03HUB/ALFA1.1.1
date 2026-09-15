615d8f49f41b5022f8d8136fe369f520a28b0124
AndreaDlaz <tecnologiamq12@gmail.com>
2026-09-15 05:57:49 +0200

    init
    commi

---
+++ alfa_pr_1/prueba/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World Andrea");
+        }
+    }
+}
